7b79ed17168775ecbd390d8f4f60ec20cb000fbb
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-21 14:54:24 -0400

    fixed db name

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,10 +17,10 @@ export const putDb = async (content) => {
   console.log("Update the database");
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB("jate", 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction("jate", "readwrite");
+  const tx = jateDb.transaction("jate", "readwrite");
 
   // Open up the desired object store.
   const store = tx.objectStore("jate");
@@ -38,10 +38,10 @@ export const getDb = async () => {
   console.log("GET from the database");
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB("jate", 1);
+  const jateDb = await openDB("jate", 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction("jate", "readonly");
+  const tx = jateDb.transaction("jate", "readonly");
 
   // Open up the desired object store.
   const store = tx.objectStore("jate");
