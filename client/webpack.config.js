2eaec5b8401292bb1280feb0756d5eb7e5b556b9
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-09-21 11:44:37 -0400

    added babel plug-in, fixed name and description

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,9 +34,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "Jate Text-Editor",
+        short_name: "Jate",
+        description: "Your cool text editor",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "./",
